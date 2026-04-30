00bd3f0b469e44f9b693b295a2805ca02354faf0
ashxvegeta <ashutoshroy453@gmail.com>
2026-04-30 22:16:50 +0530

    Project Model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -7,4 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     //
+
+    protected $fillable = [
+        'tenant_id',
+        'name'
+    ];
+
+    public function tenant()
+    {
+    return $this->belongsTo(Tenant::class);
+    }
+
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
