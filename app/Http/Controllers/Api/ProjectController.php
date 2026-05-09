bb60afbc749932bad4b902e1b28cbf152508b20f
ashxvegeta <ashutoshroy453@gmail.com>
2026-05-09 20:18:08 +0530

    Create Project

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    //
+    public function inex(Request $request)
+    {
+        $request->validate([
+            'name'=>'required|string|max:255',
+        ]);
+
+        $tenant = app('currentTenant');
+        $project = Project::create([
+            'tenant_id' => $tenant->tenant_id,
+            'name' => $request->name,
+        ]);
+
+        return response()->json([
+            'message' => 'Project created successfully',
+            'project' => $project
+        ]);
+        
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,14 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProjectController;
 
 Route::post('/register', [AuthController::class, 'register']);
 
-Route::middleware(['auth:sanctum', 'set.tenant'])->get('/test-tenant', function () {
-   return response()->json(['tenant' => app('currentTenant')]);
+Route::middleware(['auth:sanctum', 'set.tenant'])->group(function () {
+   
+   Route::get('/test-tenant', function () {
+      return response()->json([ 'tenant' => app('currentTenant')]);
+   });
+   Route::post('/projects', [ProjectController::class, 'store']);
 });
