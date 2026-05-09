f98d012ca15414e33431982dc9e62cec2d27b68a
ashxvegeta <ashutoshroy453@gmail.com>
2026-05-09 12:23:46 +0530

    Register  middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,8 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
-    })
+
+        $middleware->alias([
+            'set.tenant' => App\Http\Middleware\SetTenant::class,
+        ]);
+
+       })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
